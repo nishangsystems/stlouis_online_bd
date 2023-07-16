34ddf831212c801cb05708d4fd35b6063407d347
germanusK <germanuskeming@gmail.com>
2023-07-16 21:33:02 +0100

    lesser tweek

--- app/Http/Controllers/Admin/ProgramController.php
+++ app/Http/Controllers/Admin/ProgramController.php
@@ -1174,9 +1174,9 @@ class ProgramController extends Controller
             if(($programs = json_decode($this->api_service->programs())->data) != null){
                 $program = collect($programs)->where('id', $application->program_first_choice)->first()??null;
                 if($program != null){
-                    dd($program);
+                    // dd($program);
                     $year = substr(Batch::find(Helpers::instance()->getCurrentAccademicYear())->name, 2, 2);
-                    $prefix = $program->prefix;//3 char length
+                    $prefix = $program->prefix??null;//3 char length
                     $max_count = '';
                     if($prefix == null){
                         return back()->with('error', 'Matricule generation prefix not set.');
