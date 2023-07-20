57298a0acc2dbf4eb2efcdf174427eb3279d93a1
germanusK <germanuskeming@gmail.com>
2023-07-20 20:54:54 +0100

    light updates on form download

--- resources/views/student/online/download_form.blade.php
+++ resources/views/student/online/download_form.blade.php
@@ -1,8 +1,8 @@
 @extends('student.layout')
 @section('section')
     <div class="py-3">
-        <table class="border-left border-right shadow">
-            <thead><tr class="text-capitalize border-bottom bg-light">
+        <table class="border-left border-right shadow shadow-dark">
+            <thead class="bg-light"><tr class="text-capitalize border-bottom">
                 <th class="border-left border-right">{{ __('text.sn') }}</th>
                 <th class="border-left border-right">{{ __('text.word_year') }}</th>
                 <th class="border-left border-right">{{ __('text.word_program') }}</th>
@@ -15,7 +15,7 @@
                     <tr class="border-bottom">
                         <td class="border-left border-right">{{ $k++ }}</td>
                         <td class="border-left border-right">{{ $appl->year->name }}</td>
-                        <td class="border-left border-right">{{ $appl->name }}</td>
+                        <td class="border-left border-right">{{ $programs->where('id', $appl->program_first_choice)->first()->name .' / '.$programs->where('id', $appl->program_second_choice)->first()->name??'' }}</td>
                         <td class="border-left border-right d-flex flex-wrap">
                             <form method="post" action="{{ route('student.application.form.download', $appl->id) }}" target="new">@csrf
                                 <input type="submit" class="btn btn-xs btn-primary mx-2" value="{{ __('text.word_download') }}">
