f5b88d9a0e447c8f49a4a4405cf0a833ec16c73b
germanusK <germanuskeming@gmail.com>
2024-01-23 15:37:03 +0100

    ----

--- app/Http/Controllers/Admin/ProgramController.php
+++ app/Http/Controllers/Admin/ProgramController.php
@@ -1004,6 +1004,7 @@ class ProgramController extends Controller
             $data['title'] = "Print Student Application Form";
             $data['_this'] = $this;
             $data['action'] = __('text.word_print');
+            $data['download'] = __('text.word_download');
             $data['applications'] = ApplicationForm::whereNotNull('transaction_id')->get();
             return view('admin.student.applications', $data);
         }
