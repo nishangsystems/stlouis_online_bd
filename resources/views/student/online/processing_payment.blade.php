0283de732ff9ce8c5df00fe62f2f6e0643197dcd
germanusk <germanuskeming@gmail.com>
2025-07-04 13:49:16 +0100

    updated tranzak payment processing view to correct post-payment redirecting issue

--- resources/views/student/online/processing_payment.blade.php
+++ resources/views/student/online/processing_payment.blade.php
@@ -18,7 +18,7 @@
 </div>
 @endsection
 @section('script')
-<script>
+{{-- <script>
 
     // Create a spinner animation
     let colors = ['#fff', '#ea8', '#2f7', '#a7d', '#d9e', '#a2f'];
@@ -67,6 +67,64 @@
         });
     }, 5000);
 
+
+    if("{{ $payment_url??null }}" != null){
+        // alert("{{ $payment_url }}");
+        // let anchor = `<form id="redirector" method="get" action="{{ $payment_url }}" target="new"></form>`;
+        // $('#_temp_use').html(anchor);
+        // $('#redirector').submit();
+        window.open("{{ $payment_url }}", '_blank');
+    }
+</script> --}}
+<script>
+
+    // Create a spinner animation
+    let colors = ['#fff', '#ea8', '#2f7', '#a7d', '#d9e', '#a2f'];
+    let percentages = [0, 50, 58, 70, 88, 99];
+    let gap = 10;
+    setInterval(function(){
+        let bg = `conic-gradient(${colors[0]} ${(percentages[0]+gap-10)%100}% ${(percentages[0]+gap)%100}%, ${colors[1]} ${(percentages[1]+gap-10)%100}% ${(percentages[1]+gap)%100}%, ${colors[2]} ${(percentages[2]+gap-10)%100}% ${(percentages[2]+gap)%100}%, ${colors[3]} ${(percentages[3]+gap-10)%100}% ${(percentages[3]+gap)%100}%, ${colors[4]} ${(percentages[4]+gap-10)%100}% ${(percentages[4]+gap)%100}%, ${colors[5]} ${(percentages[5]+gap-10)%100}% ${(percentages[5]+gap)%100}% )`;
+        document.getElementById('outer_wall').style.backgroundImage = bg;
+        // console.log(bg);
+        gap += 3;
+    }, 200);
+
+    // check for the transaction status every 3s
+    set_interval = setInterval(() => {
+        _url = "{{config('tranzak.base').config('tranzak.transaction_details').$transaction->requestId}}";
+        headers = { 'Access-Control-Allow-Origin': '*',  'Authorization' : "Bearer {{ cache($tranzak_credentials->cache_token_key) }}"};
+        $.ajax({
+            method: 'get',
+            url: _url,
+            headers: headers,
+            success: function(data){
+                // check if status is completed or failed
+                response_data = data.data
+                console.log(response_data);
+                if((response_data.status == "SUCCESSFUL") || (response_data.status == "CANCELLED") || (response_data.status == "FAILED") || (response_data.status == "REVERSED")){
+                    url = "{{route('student.application.payment.complete', $form_id)}}";
+                    let form_markup = `<form method="post" id="_temp_form" action="${url}" enctype="application/json">
+                        @csrf`;
+                    console.log(Object.entries(response_data));
+                    Object.entries(response_data).forEach(element => {
+                        if(element[1] !=null && typeof element[1] === "object"){
+                            Object.entries(element[1]).forEach(sub_el =>{
+                                form_markup += `<input type="hidden" name="${element[0]}[${sub_el[0]}]" value="${sub_el[1]}">`;
+                            });
+                        }else{
+                            form_markup += `<input type="hidden" name="${element[0]}" value="${element[1]}">`;
+                        }
+                    });
+                    form_markup += `</form>`;
+                    
+                    let form = $('#_temp_use').html(form_markup);
+                    clearInterval(set_interval);
+                    $('#_temp_form').submit();
+                    // window.location = url;
+                }
+            }
+        });
+    }, 5000);
 
     if("{{ $payment_url??null }}" != null){
         // alert("{{ $payment_url }}");
